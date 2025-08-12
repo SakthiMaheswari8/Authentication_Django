c6140bc75568ef4e1775a58a77076c6cbe330d0f
SakthiMaheswari8 <shakthimaheswarip9802@gmail.com>
2025-08-12 19:14:54 +0530

    Authentication

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 urlpatterns = [
     path('',views.home,name='home'),
     path('signup/',views.signup,name='signup'),
     path('login',views.login,name='login'),
+    # path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- login/views.py
+++ login/views.py
@@ -3,11 +3,18 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .models import Authentication
 from django.contrib.auth.hashers import make_password,check_password
-from django.core.validators import RegexValidator
 import re
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
+from jwt import JWT
+from jwt.jwk import OctetJWK
+from datetime import timedelta,datetime,timezone
 
+secret_key = "TGDYIUHKSJUGDYBUISBYVDY"
+jwk_key = OctetJWK(secret_key.encode())
+
+# Create JWT instance
+jwt_instance = JWT()
 # Create your views here.
 def home(request):
     return HttpResponse("Hello all")
@@ -52,14 +59,45 @@ def login(request):
             if not all([email, password]):
                 return JsonResponse({'error': 'Email and password are required'}, status=400)
             user = Authentication.objects.filter(email=email,).first()
+            
+            # Define payload
             if user and check_password(password, user.password):
-                return JsonResponse({'message': 'Login successful', 'userid': user.id}, status=200)
+                access_payload = {
+                    "email": user.email,
+                    "iat": int(datetime.now(timezone.utc).timestamp()),
+                    "exp": int((datetime.now(timezone.utc) + timedelta(hours=1)).timestamp())
+                }
+                # Encode token
+                access_token = jwt_instance.encode(
+                    payload=access_payload,
+                    key=jwk_key,
+                    alg="HS256",
+                    optional_headers={"typ": "JWT"}
+                )
+                refresh_payload = {
+                    "email":user.email,
+                    "iat": int(datetime.now(timezone.utc).timestamp()),
+                    "exp": int((datetime.now(timezone.utc) + timedelta(days=1)).timestamp())
+                }
+                refresh_token=jwt_instance.encode(
+                    payload=refresh_payload,
+                    key=jwk_key,
+                    alg="HS256",
+                    optional_headers={"typ":"JWT"}
+                )
+                return JsonResponse({'message':'login process completed','access_token':access_token,'refresh_token':refresh_token,'userid':user.id})
             else:
-                return JsonResponse({'error': 'Invalid credentials','status id':'401'}, status=401)
+                return JsonResponse({'error': 'Invalid credentials','status id':'401'}, status=401)        
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON','status id':'400'}, status=400)
     else:
         return JsonResponse({'error': 'Only POST method allowed','status id':'405'}, status=405)
+
+
+                
+        
+    
+
 
   
 
